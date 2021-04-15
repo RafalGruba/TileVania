4f293356ce26ba0b07dd42996b704806b6ab8ed8
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-04-15 13:29:15 +0200

    animation state changes in code

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Config
     [SerializeField] float runSpeed = 5f;
-    Rigidbody2D myRigidBody;
 
-    // Start is called before the first frame update
+    // State
+    bool isAlive = true;
+
+    // Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +32,9 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal"); // value is between -1 and 1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+
     }
 
     private void FlipSprite()
