516d1d3429899bb7a46507a14c355c5d50dca2f3
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-04-15 13:00:34 +0200

    Slipping player sprite when moving left

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -24,6 +25,15 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal"); // value is between -1 and 1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 
 }
